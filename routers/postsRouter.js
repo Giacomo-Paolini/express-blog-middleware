84f91451be489b741f474d9e7e157ce27a63c41b
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-15 18:33:00 +0100

    fix

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const postsController = require('../controllers/postsController');
 const multer = require("multer");
+const authenticate = require("../middlewares/authenticate");
 
 router.get("/", postsController.index);
 router.get("/create", postsController.create);
 router.get("/:slug", postsController.show);
 router.get("/:slug/download", postsController.download);
 
-router.post("/", multer().none(), postsController.store);
+router.post("/", authenticate, postsController.store);
 router.post("/:slug", postsController.destroy);
 
 module.exports = router;
